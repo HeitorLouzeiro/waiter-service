ac2d6d6c8d57a55bed2da7b6f10b321763fdf493
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-04-10 17:41:34 -0300

    creating customer APP.

--- settings/urls.py
+++ settings/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('waiterservice.urls')),
     path('accounts/', include('accounts.urls')),
+    path('customers/', include('customers.urls')),
 ]
--- waiterservice/admin.py
+++ waiterservice/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Commands, ItemMenu, ItemPedido, Table
+
+
+class CommandsAdmin(admin.ModelAdmin):
+    list_display = ('code', 'Table', 'opening', 'closed', 'status', 'amount')
+    list_filter = ('status', 'Table')
+    search_fields = ('Table__number',)
+
+
+admin.site.register(Table)
+admin.site.register(Commands, CommandsAdmin)
+admin.site.register(Category)
+admin.site.register(ItemMenu)
+admin.site.register(ItemPedido)
