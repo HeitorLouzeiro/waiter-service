f35207374bc284f547d924a7b4cd96ec5ec35d75
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-05-12 15:47:25 -0300

    created features on the waiter's page.

--- waiter/urls.py
+++ waiter/urls.py
@@ -6,4 +6,10 @@ app_name = 'waiter'
 
 urlpatterns = [
     path('', views.panelView, name='panelview'),
+    path('perform-service/<int:task_id>/',
+         views.performService, name='performService'),
+    path('new-order/', views.newOrder, name='newOrder'),
+    path('new-order/<slug:table_slug>/', views.newOrder, name='newOrderTable'),
+    path('confirm-order/', views.confirmOrder, name='confirmOrder'),
+    path('create-order/', views.createOrder, name='createOrder'),
 ]
--- waiter/views.py
+++ waiter/views.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from collections import namedtuple
 
-from .models import Task
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Q
+from django.shortcuts import redirect, render
+
+from customers.models import Category, Commands, ItemMenu, ItemOrder, Table
+
+from .models import Delivery, Task
 
 
 # Create your views here.
@@ -9,10 +14,11 @@ from .models import Task
 def panelView(request):
     template = 'waiter/pages/panel.html'
 
-    deliveries = Task.objects.filter(type='deliver')
-    prim_attendances = Task.objects.filter(type='prim_attendance')
-    services = Task.objects.filter(type='service')
-    closures = Task.objects.filter(type='closed')
+    deliveries = Delivery.objects.all()
+    prim_attendances = Task.objects.filter(
+        type='prim_attendance', status="pending")
+    services = Task.objects.filter(type='service', status="pending")
+    closures = Task.objects.filter(type='closed', status="pending")
     qtd_services = prim_attendances.count() + services.count()
 
     context = {'deliveries': deliveries,
@@ -23,3 +29,75 @@ def panelView(request):
                }
 
     return render(request, template, context)
+
+
+@login_required(login_url='accounts:loginUser', redirect_field_name='next')
+def performService(request, task_id):
+    waiter = request.user
+    task = Task.objects.get(id=task_id)
+    task.attend_task(waiter)
+    return redirect('waiter:newOrderTable', table_slug=task.table.slug)
+
+
+@login_required(login_url='accounts:loginUser', redirect_field_name='next')
+def newOrder(request, table_slug=None):
+    template = 'waiter/pages/waitermenu.html'
+    table = None
+    if table_slug is not None:
+        table = Table.objects.get(slug=table_slug)
+
+    categories = Category.objects. \
+        annotate(active=Count('itemmenu',
+                              filter=Q(itemmenu__active=True))). \
+        filter(active__gt=0)
+
+    uncategorizedItems = ItemMenu.objects.filter(
+        category__isnull=True, active=True)
+
+    context = {'table': table,
+               'categories': categories,
+               'uncategorizedItems': uncategorizedItems
+               }
+    return render(request, template, context)
+
+
+def confirmOrder(request):
+    Order = namedtuple('Order', ['id', 'item', 'amount'])
+    orders = []
+    numTable = int(request.POST.get('num_mesa'))
+    table = Table.objects.get(number=numTable)
+    for item_id, amount in request.POST.items():
+        try:
+            id = int(item_id)
+        except ValueError:
+            pass
+        else:
+            if int(amount) > 0:
+                orders.append(
+                    Order(id, ItemMenu.objects.get(id=id), int(amount)))
+
+    return render(request, 'waiter/pages/confirmorder.html', {'orders': orders, 'table': table})
+
+
+def createOrder(request):
+    orderDict = request.POST.copy()
+    numTable = int(orderDict.get('num_mesa'))
+    table = Table.objects.get(number=numTable)
+    commands = Commands.objects.get(Table=table, status='open')
+    del orderDict['num_mesa']
+    del orderDict['csrfmiddlewaretoken']
+
+    for item_id, amount in orderDict.items():
+        itemmenu = ItemMenu.objects.get(id=int(item_id))
+        print(itemmenu)
+        for i in range(int(amount)):
+            newOder = ItemOrder(
+                item=itemmenu,
+                price=float(itemmenu.price),
+                commands=commands,
+                status='preparation' if itemmenu.needs_preparation else 'ready',
+            )
+            newOder.save()
+
+    commands.update_total()
+    return redirect('waiter:panelview')
