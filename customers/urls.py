83a0dcbf6063a117e07cc99954ca6e203424cc6f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-05-23 11:44:34 -0300

    Removed unnecessary files.

--- customers/urls.py
+++ customers/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'customers'
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('table/<slug:slug>/', views.clientDesk, name='clientDesk'),
     path('customermenu/<slug:slug>/', views.customermenu, name='customermenu'),
     path('request-service/<slug:slug>/',
